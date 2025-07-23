2c6e213efcf161be3a75e223933162226aa9250b
baboribo <inertia_mailer_0c@icloud.com>
2025-07-23 23:56:01 +0900

    chore: 기존 이미지를 iframe 임베드로 변경함. (YouTube 임베드) 기존 Link 이미지는 주석처리됨. build: 모듈 업데이트 및 보안 업데이트함.

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div style={{ display: "flex", alignItems: "center", flexDirection: "column", height: '100%' }}>
-      <Link href="https://youtu.be/JDpkvycpqaQ?si=P1uaI-GzTUx_KxlJ">
+      {/* <Link href="https://youtu.be/JDpkvycpqaQ?si=P1uaI-GzTUx_KxlJ">
         <Image src="/baka.png" width={400} height={400} alt="Baka Cirno" className='rounded-xl' />
-      </Link>
+      </Link> */}
+      <iframe
+        src="https://www.youtube.com/embed/JDpkvycpqaQ?si=P1uaI-GzTUx_KxlJ"
+        title="YouTube video player"
+        className='rounded-xl'
+        style={{ width: '100%', height: '500px', maxWidth: '800px', marginTop: '20px' }}
+        allow="accelerometer; autoplay; subtitle; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen />
     </div>
   );
 }
